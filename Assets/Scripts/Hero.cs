9cacb243b253841d93915567ced5cc9a1061b934
kxh337 <khitomi337@gmail.com>
2014-07-12 15:55:38 -0400

    created basic scripts for units
    added enemy and hero scripts where the heroes are the players units.
    Also added in a basic unit script that will apply combat math system to
    both enemies and heroes

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : Unit {
+	public float stamina;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void focus(){
+		int recovery = (int)stamina/10;
+		stamina += recovery;
+		Mathf.Clamp(stamina,0,100);
+	}
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+	public float health;
+	public float strength;
+	public float speed;
+	public float defense;
+	public float stamina;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void recieveDmg(float dmg){
+		dmg -= defense;
+		health -= dmg;
+	
+	}
+
+	public void attack(Unit enemy){
+		bool crit=false;
+		if(Random.Range(0,100)<25){
+			crit = true;
+		}
+		float dmg  = (int)Random.Range(strength*.85f,strength*1.1f);
+		if(crit){
+			dmg = (int)(dmg * 1.5f);
+		}
+		enemy.recieveDmg(dmg);
+	}
+
+
+
+}
+
